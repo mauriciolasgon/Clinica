f06198c26869c18fd54dfd4c0afb077129dc783c
Gonzaloroque200407 <gonzaloroque200407@gmail.com>
2024-05-28 00:33:34 -0300

    dashboardpacientes

--- laravel/resources/js/Layouts/AuthenticatedLayout.vue
+++ laravel/resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
@@ -21,8 +20,10 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                                    <img
+                                        src="@/Pages/images/fotoxastre.jpeg"
+                                        alt="Xames Logo"
+                                        class="block h-12 w-auto"
                                     />
                                 </Link>
                             </div>
